00b696c314d237ed13c910a42bd852d1afe85c15
Mohammed Sanusi <sanusi@snapnet.com.ng>
2019-03-15 16:36:19 +0100

    Small Vue work

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+	<nav class="panel">
+		
+  <p class="panel-heading">
+    PhoneBook
+  </p>
+  <button class="button is-primary is-outlined is-fullwidth">
+      reset all filters
+    </button>
+  <div class="panel-block">
+    <p class="control has-icons-left">
+      <input class="input is-small" type="text" placeholder="search">
+      <span class="icon is-small is-left">
+        <i class="fas fa-search" aria-hidden="true"></i>
+      </span>
+    </p>
+  </div>
+  <a class="panel-block">
+    <span class="panel-icon">
+      <i class="fas fa-code-branch" aria-hidden="true"></i>
+    </span>
+    mojs
+  </a>
+  <div class="panel-block">
+    
+  </div>
+</nav>
+</template>
---
+++ resources/views/phonebook.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PhoneBook</title>
+	<link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+</head>
+<body>
+
+<div id="app">
+		
+<Heading></Heading>
+<Home></Home>
+</div>
+<script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
